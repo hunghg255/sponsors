fce01ca33f6b3ca24bc16bfacb8a2518dc32df68
hunghg255 <giahunghust@gmail.com>
2023-10-15 11:21:27 +0700

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+## Sponsors
+
+[![Scheduler](https://github.com/hunghg255/sponsors/actions/workflows/scheduler.yml/badge.svg)](https://github.com/hunghg255/sponsors/actions/workflows/scheduler.yml)
+
+If my code has helped you, please consider [sponsoring me](https://github.com/sponsors/hunghg255).
+
+- <img src="./icons/sponsor.svg"> [GitHub Sponsor](https://github.com/sponsors/hunghg255)
+
+Thank you for your support!
+
+
+## Contact
+
+- Twitter: [@hunghg255](https://twitter.com/hunghg255)
+
+<p align="center">
+  <a href="./sponsors.svg">
+    <img src='./sponsors.svg'/>
+  </a>
+</p>
+
+## Thanks
+
+Built with [SponsorKit](https://github.com/antfu/sponsorkit).
---
+++ sponsorkit.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  formats: ['svg', 'json'],
+  afdian: {
+    exechangeRate: 7.2,
+  },
+  tiers: [
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: presets.xs,
+    },
+    {
+      title: '❤️ Backers',
+      preset: presets.base,
+    },
+    {
+      title: '💖 Sponsors',
+      monthlyDollars: 8,
+      preset: presets.medium,
+      composeAfter: (composer, tierSponsors, config) => {
+        composer.addSpan(10)
+      },
+    },
+    {
+      title: '💓 Bronze Sponsors',
+      monthlyDollars: 32,
+      preset: presets.large,
+    },
+    {
+      title: '💗 Silver Sponsors',
+      monthlyDollars: 64,
+      preset: presets.xl,
+    },
+    {
+      title: '💞 Gold Sponsors',
+      monthlyDollars: 256,
+      preset: presets.xl,
+    },
+  ],
+})
